d2398bdba3894a6234c7630760f0046416324158
Cynthia <kalechecynthia@gmail.com>
2024-10-09 14:55:25 +0300

    works on lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+function findMatching(driversArray, driver) {
+  return driversArray.filter((name) => {
+    return name.toLowerCase() === driver.toLowerCase();
+  });
+}
+
+function fuzzyMatch(driversArray, driverString) {
+  return driversArray.filter((name) => {
+    return name.toLowerCase().startsWith(driverString.toLowerCase());
+  });
+}
+
+const driversHometowns = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(driversObjectsInArray, driverString) {
+  return driversObjectsInArray.filter((driver) => {
+    return driver.name.toLowerCase() === driverString.toLowerCase();
+  });
+}
